2ac5ee68fe21896207cd367f9324a964142e66de
Kritsaphong Saengsawang <66160290@go.buu.ac.th>
2025-01-27 22:22:27 +0700

    convert celsius to fahrenheit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('hello')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
@@ -33,5 +33,29 @@ export class AppController {
   @Post('test-body')
   testBody(@Req() req, @Body() body, @Body('celsius') celsius : number) {
     return { celsius };
+  }
+
+  @Get('convert')
+  convert(@Query('celsius') celsius: string){
+    return {
+      celsius: parseFloat(celsius), 
+      fahrenheit: (parseFloat(celsius) * 9.0) / 5 + 32,
+    };
+  }
+
+  @Post('convert')
+  convertByPost(@Body('celsius') celsius: number){
+    return {
+      celsius: celsius, 
+      fahrenheit: (celsius * 9.0) / 5 + 32,
+    };
+  }
+
+  @Get('convert/:celsius')
+  convertParam(@Param('celsius') celsius: string){
+    return {
+      celsius: parseFloat(celsius), 
+      fahrenheit: (parseFloat(celsius) * 9.0) / 5 + 32,
+    };
   }
 }
