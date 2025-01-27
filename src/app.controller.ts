4c6452c1a512c04b8eb55d4f9b6c996bee230ec1
Kritsaphong Saengsawang <66160290@go.buu.ac.th>
2025-01-27 19:45:04 +0700

    Hello Buu

---
+++ src/app.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('hello')
+  getHello(): string {
+    return '<html><body><h1>Hello Buu</h1><body><html>';
+  }
+}
