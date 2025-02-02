b40ec7ea87251e7c9d295690bee9133c3fde1bfe
Kritsaphong Saengsawang <66160290@go.buu.ac.th>
2025-02-02 21:51:33 +0700

    mockup user 1

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,7 @@
-export class User {}
+export class User {
+    id: number;
+    login: string;
+    password: string;
+    roles: ("admin" | "user")[];
+    gender: "male" | "female";
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  users: User[] = [{
+    id: 1,
+    login: 'admin',
+    password: 'Pass@1234',
+    roles: ['admin'],
+    gender: 'male',
+  }];
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.users;
   }
 
   findOne(id: number) {
